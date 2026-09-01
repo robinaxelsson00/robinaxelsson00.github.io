aebaa4a4f439cb5c636292d1702e3f983f28d1be
robinaxelsson00 <robinaxelsson00@gmail.com>
2026-09-01 13:17:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
         cursor.style.top = `${e.clientY}px`;
     });
 
-    // Subtil förstoring/effekt när man hovrar över klickbara element
+    // Subtil förstoring vid hovring på länkar
     const interactiveElements = document.querySelectorAll('a, button');
     
     interactiveElements.forEach(el => {
         el.addEventListener('mouseenter', () => {
-            cursor.style.transform = 'translate(-50%, -50%) scale(1.5)';
+            cursor.style.transform = 'translate(-50%, -50%) scale(1.4)';
         });
         
         el.addEventListener('mouseleave', () => {
